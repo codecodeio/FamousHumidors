e7d53cee9227e3db10af1d3f3feeaa9755b21bca
Matt Code <matt@codecode.io>
2018-04-30 13:17:46 -0700

    Add project files.

---
+++ FamousHumidors/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FamousHumidors.MvcApplication" Language="C#" %>
---
+++ FamousHumidors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FamousHumidors.Startup))]
+namespace FamousHumidors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
